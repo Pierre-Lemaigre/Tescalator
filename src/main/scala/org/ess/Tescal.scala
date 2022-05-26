fa53a40323ab499c602b9e74841b9e901d379888
Pierre-Lemaigre <pierrelemaigre.pro@gmail.com>
2022-05-26 20:51:43 +0200

    More scala

--- src/main/scala/org/ess/Tescal.scala
+++ src/main/scala/org/ess/Tescal.scala
@@ -5,24 +5,25 @@ import scala.annotation.tailrec
 @main def Tescalor(args: String*): Unit =
   println(args.mkString(", "))
   println(Tescal.sumLength(args))
+  println(Tescal.mean(Seq(5, 23, 23.5)))
 
 
 object Tescal:
-  def sumLength(strs: Seq[_]): Int =
+  def sumLength(sq: Seq[_]): Int =
     @tailrec
-    def sum(strs: Seq[_], acc: Int): Int = strs match
+    def sum(sq: Seq[_], acc: Int): Int = sq match
       case Nil => acc
-      case _ :: tail => sum(tail, acc+1)
+      case _ :: tail => sum(tail, acc + 1)
 
-    sum(strs, 0)
+    sum(sq, 0)
 
-  def sumCollection(strs: Seq[Double]): Double =
+  def sumCollection(sq: Seq[Double]): Double =
     @tailrec
-    def sum(strs: Seq[Double], acc: Double): Double = strs match
+    def sum(sq: Seq[Double], acc: Double): Double = sq match
       case Nil => acc
-      case hd :: tail => sum(tail, acc+hd)
+      case hd :: tail => sum(tail, acc + hd)
 
-    sum(strs, 0)
+    sum(sq, 0)
 
   def mean(sq: Seq[Double]): Double =
     sumCollection(sq) / sumLength(sq)
