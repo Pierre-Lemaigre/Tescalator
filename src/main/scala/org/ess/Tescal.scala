f944252b579a723849236f5a962bddc1b8948947
Pierre-Lemaigre <pierrelemaigre.pro@gmail.com>
2022-05-26 20:43:55 +0200

    Code

--- src/main/scala/org/ess/Tescal.scala
+++ src/main/scala/org/ess/Tescal.scala
@@ -4,14 +4,25 @@ import scala.annotation.tailrec
 
 @main def Tescalor(args: String*): Unit =
   println(args.mkString(", "))
-  println(Tescal.sumCollection(args))
+  println(Tescal.sumLength(args))
 
 
 object Tescal:
-  def sumCollection(strs: Seq[_]): Int =
+  def sumLength(strs: Seq[_]): Int =
     @tailrec
     def sum(strs: Seq[_], acc: Int): Int = strs match
       case Nil => acc
       case _ :: tail => sum(tail, acc+1)
 
     sum(strs, 0)
+
+  def sumCollection(strs: Seq[Double]): Double =
+    @tailrec
+    def sum(strs: Seq[Double], acc: Double): Double = strs match
+      case Nil => acc
+      case hd :: tail => sum(tail, acc+hd)
+
+    sum(strs, 0)
+
+  def mean(sq: Seq[Double]): Double =
+    sumCollection(sq) / sumLength(sq)
